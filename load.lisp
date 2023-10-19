55d9c9cf5fa90420b2eda6b1185d295a591b5bcc
Roy M. Turner <rturner@maine.edu>
2023-10-19 14:43:16 -0400

    Got rid of some of the warnings, etc.

---
+++ load.lisp
@@ -0,0 +1,7 @@
+(in-package cl-user)
+
+(declaim (sb-ext:muffle-conditions warning))
+(declaim (sb-ext:muffle-conditions style-warning))
+(load "simulator")
+(declaim (sb-ext:unmuffle-conditions style-warning))
+(declaim (sb-ext:unmuffle-conditions warning))
